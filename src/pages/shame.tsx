75093478b4f8b251905df27ca813405798892bfc
Patrik Rizell <patrik.rizell@tunstall.com>
2023-04-17 18:08:32 +0200

    Initial commit.

---
+++ src/pages/shame.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Shame() {
+  return (
+    <div className='w-screen h-screen bg-gradient-to-b #487a99 #39617a'>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='text-primary font-bold text-6xl mt-12'>Shame!!!</div>
+        <div className='text-primary font-bold text-2xl mt-12'>
+          You should be focusing on work right now
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
+const focusColor = "#487a99";
+const focusDarkColor = "#39617a"; // lighter #406d89
+const breakColor = "#e69a38";
+const breakDarkColor = "#b87b2c"; // lighter #cf8a32
+const primary = "#f2e6d8";
+const primaryDark = "#d9cfc2"; //lighter #d9cfc2, darker #c1b8ac
+const secondary = "#80363d";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        //https://lospec.com/palette-list/bitbybit
+        focus: focusColor,
+        break: breakColor,
+        primary,
+        "primary-dark": primaryDark,
+        secondary,
+      },
+      textColor: {
+        focus: focusColor,
+        "focus-dark": focusDarkColor,
+        break: breakColor,
+        "break-dark": breakDarkColor,
+        primary: primary,
+        "primary-dark": primaryDark,
+        secondary: secondary,
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
